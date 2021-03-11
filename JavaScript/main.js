e709ac721ada9b7bf9646beebc9b23650b93070e
NiqueMA <77253680+NiqueMA@users.noreply.github.com>
2021-03-11 14:29:24 -0500

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,111 @@
+
+   
+   /*var mouth = ["mouth1.png", "mouth2.png", "mouth3.png", "mouth4.png", "mouth5.png"];
+
+   function randomize()
+   {
+       console.log("Button was pressed")
+       var DogArray = Math.floor(Math.random() * mouth.length);
+       // document.getElementById("dog").src = mouth[DogArray];
+       
+
+   }
+     
+*/
+
+
+// eye options 
+
+function longEyes()
+{
+    document.getElementById("eye").src = "eye1.png";
+    x.play(); 
+}
+
+function roundEyes()
+{
+    document.getElementById("eye").src = "eye2.png";
+}
+function blackEyes()
+{
+    document.getElementById("eye").src = "eye3.png";
+}
+function scaredEyes()
+{
+    document.getElementById("eye").src = "eye4.png";
+}
+function sadEyes()
+{
+    document.getElementById("eye").src = "eye5.png";
+}
+
+// mouth options
+
+function bigMouth()
+{
+    document.getElementById("mouth").src = "mouth1.png";
+}
+
+function tongueMouth()
+{
+    document.getElementById("mouth").src = "mouth2.png";
+}
+function yellMouth()
+{
+    document.getElementById("mouth").src = "mouth3.png";
+}
+function scaredMouth()
+{
+    document.getElementById("mouth").src = "mouth4.png";
+}
+function puckerMouth()
+{
+    document.getElementById("mouth").src = "mouth5.png";
+}
+// nose options
+
+function bigNose()
+{
+    document.getElementById("nose").src = "nose1.png";
+}
+
+function longNose()
+{
+    document.getElementById("nose").src = "nose2.png";
+}
+function clownNose()
+{
+    document.getElementById("nose").src = "nose3.png";
+}
+function nostrilNose()
+{
+    document.getElementById("nose").src = "nose4.png";
+}
+function basicNose()
+{
+    document.getElementById("nose").src = "nose5.png";
+
+}
+// hair options
+
+function shortHair()
+{
+    document.getElementById("hair").src = "hair1.png";
+}
+
+function tailHair()
+{
+    document.getElementById("hair").src = "hair2.png";
+}
+function wavyHair()
+{
+    document.getElementById("hair").src = "hair3.png";
+}
+function curlyHair()
+{
+    document.getElementById("hair").src = "hair4.png";
+}
+function redHair()
+{
+    document.getElementById("hair").src = "hair5.png";
+}
